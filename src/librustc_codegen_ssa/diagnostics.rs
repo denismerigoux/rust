29ed36bb387efc8231c5ec7c891f6712fa02ed73
Denis Merigoux <denis.merigoux@gmail.com>
2018-10-03 13:49:57 +0200

    Greate separation of librsutc_codegen_llvm : librustc_codegen_ssa compiles

--- src/librustc_codegen_ssa/diagnostics.rs
+++ src/librustc_codegen_ssa/diagnostics.rs
@@ -12,38 +12,25 @@
 
 register_long_diagnostics! {
 
-E0511: r##"
-Invalid monomorphization of an intrinsic function was used. Erroneous code
-example:
+E0668: r##"
+Malformed inline assembly rejected by LLVM.
 
+LLVM checks the validity of the constraints and the assembly string passed to
+it. This error implies that LLVM seems something wrong with the inline
+assembly call.
+
+In particular, it can happen if you forgot the closing bracket of a register
+constraint (see issue #51430):
 ```ignore (error-emitted-at-codegen-which-cannot-be-handled-by-compile_fail)
-#![feature(platform_intrinsics)]
-
-extern "platform-intrinsic" {
-    fn simd_add<T>(a: T, b: T) -> T;
-}
+#![feature(asm)]
 
 fn main() {
-    unsafe { simd_add(0, 1); }
-    // error: invalid monomorphization of `simd_add` intrinsic
+    let rax: u64;
+    unsafe {
+        asm!("" :"={rax"(rax));
+        println!("Accumulator is: {}", rax);
+    }
 }
-```
-
-The generic type has to be a SIMD type. Example:
-
-```
-#![feature(repr_simd)]
-#![feature(platform_intrinsics)]
-
-#[repr(simd)]
-#[derive(Copy, Clone)]
-struct i32x2(i32, i32);
-
-extern "platform-intrinsic" {
-    fn simd_add<T>(a: T, b: T) -> T;
-}
-
-unsafe { simd_add(i32x2(0, 0), i32x2(1, 2)); } // ok!
 ```
 "##,
 
